75a1044d8a6ac10de2257e93c554e01688b6de22
Daniel Nguyen <lil.daniel92@gmail.com>
2026-07-29 14:31:08 +1000

    Pin report lineage to audited calculations

--- src/openwind_au/report_lineage.py
+++ src/openwind_au/report_lineage.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 CALCULATION_BASIS_URL = (
     "https://github.com/Elandu/OpenWind-AU/blob/"
-    "60022eb95978a8b9ca382461e859affd71de3370/docs/calculation-basis.md"
+    "4ad35b1e4c93d69a265d3a0091db3168ac0a550b/docs/calculation-basis.md"
 )
 CALCULATION_BASIS_REPORT_TEXT = (
     f"Calculation basis and data lineage reference: {CALCULATION_BASIS_URL}."
